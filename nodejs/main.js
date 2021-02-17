06c263a2060c1122f8c487b554261c0d8aa686a6
LeeSangsu <dltkdtn56@naver.com>
2021-02-17 21:40:15 +0900

    Study Node.js

--- nodejs/main.js
+++ nodejs/main.js
@@ -2,44 +2,67 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
+function templateHTML(title, list, body){
+  return `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    ${body}
+  </body>
+  </html>
+      `;
+}
+
+function makeList(filelist){
+  var list = '<ul>';
+  for(var i = 0; i<filelist.length; i++){
+    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+  }
+  list = list + '</ul>';
+  return list;
+}
+
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-    var title = queryData.id;
-    
-    if(_url == '/'){
-      title = 'Welcome';
-    }
-    if(_url == '/favicon.ico'){
-      response.writeHead(404);
-      response.end();
-      return;
-    }
-    response.writeHead(200);
-    fs.readFile(`./data/${title}`, 'utf8', function(err, description){
-        var template = `
-    <!doctype html>
-<html>
-<head>
-  <title>WEB1 - ${title}</title>
-  <meta charset="utf-8">
-</head>
-<body>
-  <h1><a href="/">WEB</a></h1>
-  <ol>
-    <li><a href="/?id=HTML">HTML</a></li>
-    <li><a href="/?id=CSS">CSS</a></li>
-    <li><a href="/?id=JavaScript">JavaScript</a></li>
-  </ol>
-  <h2>${title}</h2>
-  <p>${description}</p>
-</body>
-</html>
-    `;
+    var pathname = url.parse(_url, true).pathname;  
+    var title = queryData.id; 
 
-    response.end(template);
-    });
+    if(pathname === '/'){
+        if(title === undefined){
+          fs.readdir('./data', function(error, filelist){
+            title = 'Welcome';
+            var description = 'Hello, Node.js';
+            var list = makeList(filelist);
+            var body = `<h2>${title}</h2><p>${description}</p>`;
+            var template = templateHTML(title, list, body);
+            response.writeHead(200);
+            response.end(template);
+          });
+        } else {
+            fs.readdir('./data', function(error, filelist){
+              var list = makeList(filelist);
+            fs.readFile(`./data/${title}`, 'utf8', function(err, description){
+              var body = `<h2>${title}</h2><p>${description}</p>`;
+              var template = templateHTML(title, list, body);
+              response.writeHead(200);
+              response.end(template);
+            });
+          });
+        }
+    } else {
+      response.writeHead(404);
+      response.end('Not found');
+    }
+
     
- 
+    
+    
 });
 app.listen(3000);
