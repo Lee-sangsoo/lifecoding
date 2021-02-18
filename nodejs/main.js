92b080aa1f8e87c17324f3880c89b886005fa843
LeeSangsu <dltkdtn56@naver.com>
2021-02-18 23:37:43 +0900

    Add request on

--- nodejs/main.js
+++ nodejs/main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
 function templateHTML(title, list, body){
   return `
@@ -29,8 +30,12 @@ function makeList(filelist){
   return list;
 }
 
-var app = http.createServer(function(request,response){
-    var _url = request.url;
+// request = 요청, response = 응답
+// request = 주소창, response = 웹화면
+// request = 사용자가 입력한 내용
+// response = request에 대한 응답
+var app = http.createServer(function(request,response){ 
+    var _url = request.url;                             
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;  
     var title = queryData.id; 
@@ -62,7 +67,7 @@ var app = http.createServer(function(request,response){
         title = 'WEB - create';
         var list = makeList(filelist);
         var body = `
-          <form action="http://localhost:3000/process_create" method="post">
+          <form action="http://localhost:3000/create_process" method="post">
           <p><input type="text" name="title" placeholder="title"></p>
           <p>
               <textarea name="description" placeholder="description"></textarea>
@@ -76,6 +81,19 @@ var app = http.createServer(function(request,response){
         response.writeHead(200);
         response.end(template);
       });
+    } else if(pathname === '/create_process'){
+      var body = '';
+      request.on('data', function(data){ // 데이터가 request로 들어오는 족족 body에 덧붙인다.
+        body += data;
+      });
+      request.on('end', function(){ // 더이상 들어오는 데이터가 없다면(end) 콜백 실행
+        var post = qs.parse(body);
+        var title = post.title;
+        var description = post.description;
+      });
+
+      response.writeHead(200);
+      response.end('success');
     } else {
       response.writeHead(404);
       response.end('Not found');
